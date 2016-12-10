12814fd6965da3686ea7fcdb6de7cf7de452b2a7
rampage0131 <jake.docker0131@gmail.com>
2016-12-10 14:58:48 +0100

    menu

---
+++ Assets/_Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{ 
+    public Button StartButton;
+    public Button ExitButton;
+    public Button Startia;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Gomoku");
+            //Application.LoadLevel("Gomoku");
+        });
+        ExitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/_Scripts/resultat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class resultat : MonoBehaviour
+{
+
+    public Button ReplayButton;
+    public Button ExitButton;
+
+    void Start()
+    {
+        ReplayButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Gomoku");
+        });
+        ExitButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Menu");
+        });
+
+        if (Control.player == 1)
+
+    }
+}
