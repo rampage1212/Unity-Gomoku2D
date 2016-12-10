1906b409506d60bc7aef2d77713fbd10a0683f96
rampage0131 <jake.docker0131@gmail.com>
2016-12-10 17:32:28 +0100

    add fichier

---
+++ Assets/_Scripts/menurules.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menurules : MonoBehaviour
+{
+    public Button ExitButton;
+    public Button ContinueButton;
+    public Control control;
+
+
+    void Start()
+    {
+        ExitButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Menu");
+        });
+        ContinueButton.onClick.AddListener(() =>
+        {
+            control.Checkpaused();
+        });
+    }
+
+    public void Rules()
+    {
+
+    }
+}
