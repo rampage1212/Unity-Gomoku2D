436001ad23682cb54036bf272e1cfc2874309940
rampage0131 <jake.docker0131@gmail.com>
2016-12-12 21:17:06 +0100

    edit check.png du menu + ajout du menurules dans le mode IA

---
+++ Assets/_Scripts/menurulesIA.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menurulesIA : MonoBehaviour
+{
+    public Button ExitButton;
+    public Button ContinueButton;
+    public Button Threerules;
+    public Button Fiverules;
+    public ControlIA control;
+    public Sprite croix;
+    public Sprite box;
+
+
+    void Start()
+    {
+        Threerules.onClick.AddListener(() =>
+        {
+            if (control.b_three == true)
+            {
+                Threerules.image.sprite = box;
+                control.b_three = false;
+            }
+            else
+            {
+                Threerules.image.sprite = croix;
+                control.b_three = true;
+            }
+        });
+        Fiverules.onClick.AddListener(() =>
+        {
+            /*
+            if (control.b_five == true)
+            {
+                Fiverules.image.sprite = box;
+                control.b_five = false;
+            }
+            else
+            {
+                Fiverules.image.sprite = croix;
+                control.b_five = true;
+            }
+            */
+        });
+        ExitButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Menu");
+        });
+        ContinueButton.onClick.AddListener(() =>
+        {
+            control.Checkpaused();
+        });
+    }
+
+    public void Rules()
+    {
+
+    }
+}
